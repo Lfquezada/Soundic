58094a585731a3cef6c0076a097dbba00f99cb2e
lquezada <que18028@uvg.edu.gt>
2020-03-02 18:56:17 -0600

    Main Frame

--- Main.py
+++ Main.py
@@ -1,2 +1,19 @@
 
+import tkinter as tk
+from tkinter import filedialog,Text
+import os
+
+root = tk.Tk()
+canvas = tk.Canvas(root,height=500,width=500, bg='#171717')
+canvas.pack()
+
+frame = tk.Frame(root,bg='white')
+frame.place()
+
+streamButton = tk.Button(root,text='Stream',padx=10,pady=5,fg='#171717')
+streamButton.pack()
+
+
+root.mainloop()
+
 

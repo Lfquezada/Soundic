eec6c23bc32703a3b1a715cd2b3aeb0eca09865b
lquezada <que18028@uvg.edu.gt>
2020-03-03 16:06:41 -0600

    Minor changes

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@
 import tkinter as tk
 from tkinter import filedialog,Text
 import os
+import psycopg2
 
 root = tk.Tk()
 canvas = tk.Canvas(root,height=500,width=500, bg='#171717')
@@ -12,8 +13,6 @@ frame.place()
 
 streamButton = tk.Button(root,text='Stream',padx=10,pady=5,fg='#171717')
 streamButton.pack()
-
-
 root.mainloop()
 
 
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Soundic
+### Music Streaming Service
+UVG - Bases de Datos
+
+## Requirements
+* Python 3.x
+* psycopg2
+* tkinter
+
+## Usage
+
+```
+python3 Main.py
+```
+
+## Authors
+* **Luis Quezada** 18028
+* **Jennifer Sandoval** 18962
+* **Esteban del Valle** 18221

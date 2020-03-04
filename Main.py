489a211025b40095e2c0906b2f29c41b9777442f
lquezada <que18028@uvg.edu.gt>
2020-03-04 00:26:45 -0600

    Testing queies to test DB with psycopg2

--- Main.py
+++ Main.py
@@ -2,8 +2,9 @@
 import tkinter as tk
 from tkinter import filedialog,Text
 import os
-import psycopg2
+import psycopg2 as pg
 
+'''
 root = tk.Tk()
 canvas = tk.Canvas(root,height=500,width=500, bg='#171717')
 canvas.pack()
@@ -14,5 +15,24 @@ frame.place()
 streamButton = tk.Button(root,text='Stream',padx=10,pady=5,fg='#171717')
 streamButton.pack()
 root.mainloop()
+'''
+
+connection = pg.connect(user='postgres',host='localhost',port='5432',database='test',password='dbpass20')
+cursor = connection.cursor()
+
+# testing queries
+cursor.execute("SELECT name,artist FROM track WHERE name='Levels'")
+rows = cursor.fetchall()
+
+for r in rows:
+	print(r)
 
 
+cursor.execute("SELECT name,artist FROM track WHERE artist='Coldplay'")
+rows = cursor.fetchall()
+
+for r in rows:
+	print(r)
+
+cursor.close()
+connection.close()

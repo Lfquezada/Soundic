29874e65c29f8d58ba52c00d79491040e1ca9ca6
JennsiS <42972809+JennsiS@users.noreply.github.com>
2020-05-13 18:20:42 -0600

    Connecting the script with a mongo db

--- src/mongoscript.py
+++ src/mongoscript.py
@@ -2,7 +2,10 @@
 import psycopg2 as pg
 import json
 import pymongo
-#client = pymongo.MongoClient('mongodb://localhost:27017/')
+
+client = pymongo.MongoClient('mongodb://localhost:27017/')
+db=client.recommendations
+col = db["purchases"]
 
 connection = pg.connect(user='postgres',host='localhost',port='5432',database='Soundic',password='dbpass20')
 cursor = connection.cursor()
@@ -18,10 +21,21 @@ def purchases (date):
 			WHERE i.InvoiceDate= %s
 	     ) data;
 		"""
+	#Writing json file with purchases made on the date indicated
 	cursor.execute(query,[date])
 	rows = cursor.fetchall()
+	purchases=[]
+	for row in rows:
+		for key in cursor.description:
+			purchases.append({key[0]: value for value in row})
 	with open('purchases.json', 'w') as file:
-		json.dump(rows, file, indent=4)
+		json.dump({'purchases':purchases}, file, indent=4 )
+
+	#Reading json file and adding a document to a mongo db collection
+	data_json={}
+	with open('purchases.json', 'r') as data_file:
+	    data_json = json.load(data_file)
+	col.insert(data_json)
 
 #Writing json file with recent tracks 
 def RecentTracks():
@@ -35,11 +49,20 @@ def RecentTracks():
 	"""
 	cursor.execute(query)
 	rows = cursor.fetchall()
+	tracks=[]
+	for row in rows:
+		for key in cursor.description:
+			tracks.append({key[0]: value for value in row})
 	with open('RecentTracks.json', 'w') as file:
-		json.dump(rows, file, indent=4)
+		json.dump({'tracks':tracks}, file, indent=4 )
+
+	data_json={}
+	with open('RecentTracks.json', 'r') as data_file:
+	    data_json = json.load(data_file)
+	col.insert(data_json)
 
 
 purchases('2010/2/8')
-RecentTracks()
+
 
 

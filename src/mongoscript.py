fa9c3a6ca11d8138d39ec306863dabbf994f65b4
JennsiS <42972809+JennsiS@users.noreply.github.com>
2020-05-17 20:05:16 -0600

    Changes in mongoscript.py

--- src/mongoscript.py
+++ src/mongoscript.py
@@ -1,3 +1,4 @@
+#This python script interact with a postgresql db and allows to save a report of an specific date in a mongo db
 
 import psycopg2 as pg
 import json
@@ -5,7 +6,8 @@ import pymongo
 
 client = pymongo.MongoClient('mongodb://localhost:27017/')
 db=client.recommendations
-col = db["purchases"]
+purchases_collection = db["purchases"]
+recent_tracks_collection= db["new_tracks"]
 
 connection = pg.connect(user='postgres',host='localhost',port='5432',database='Soundic',password='dbpass20')
 cursor = connection.cursor()
@@ -13,13 +15,15 @@ cursor = connection.cursor()
 #Writing json file with purchases made on the date indicated
 def purchases (date):
 	query = """
-	     SELECT row_to_json(data) FROM (
-	      SELECT *
+	     SELECT row_to_json(purchases) FROM (
+	      SELECT i.InvoiceDate AS date, g.name AS genre, c.FirstName, c.LastName
 			FROM InvoiceLine il
 			JOIN Invoice i ON il.InvoiceId= i.InvoiceId
 			JOIN Customer c ON i.CustomerId=c.CustomerId
+			JOIN Track t ON t.TrackId=il.TrackId
+			JOIN Genre g ON g.GenreId=t.GenreId
 			WHERE i.InvoiceDate= %s
-	     ) data;
+	     ) purchases;
 		"""
 	#Writing json file with purchases made on the date indicated
 	cursor.execute(query,[date])
@@ -35,16 +39,19 @@ def purchases (date):
 	data_json={}
 	with open('purchases.json', 'r') as data_file:
 	    data_json = json.load(data_file)
-	col.insert(data_json)
+	purchases_collection.insert(data_json)
+
+
 
 #Writing json file with recent tracks 
 def RecentTracks():
 	query="""
 	SELECT row_to_json(data) FROM(
-		SELECT 
-		FROM track_register tr
-		JOIN Track t ON  t.TrackId=tr.TrackId
-		WHERE tr.Date > '2020-04-30'
+		SELECT t.TrackId, t.Name, t.AlbumId, t.MediaTypeId, t.Composer, t.Milliseconds, t.Bytes, t.UnitPrice, g.name AS genre
+		FROM track_register r
+		JOIN Track t ON  t.TrackId=r.TrackId
+		JOIN Genre g ON t.GenreId=g.GenreId
+		WHERE r.Date >= '2020-04-30'
 	)data;
 	"""
 	cursor.execute(query)
@@ -59,10 +66,10 @@ def RecentTracks():
 	data_json={}
 	with open('RecentTracks.json', 'r') as data_file:
 	    data_json = json.load(data_file)
-	col.insert(data_json)
+	recent_tracks_collection.insert(data_json)
 
 
 purchases('2010/2/8')
-
+RecentTracks()
 
 

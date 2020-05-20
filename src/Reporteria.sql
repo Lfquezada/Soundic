8690009c47df0c2ae0fa93922193a0f51b5df0c0
Estdv <35667336+Estdv@users.noreply.github.com>
2020-05-19 23:40:09 -0600

    Add ArtistRange and Documentation

--- src/Reporteria.sql
+++ src/Reporteria.sql
@@ -4,6 +4,7 @@
 
 ----------------------------------------------------
 -- FUNCION 1 PARA VENTAS POR SEMANA EN UN RANGO DADO
+-- Recibe 2 fechas
 ----------------------------------------------------
 DROP function IF EXISTS SALESWEEK;
 
@@ -19,13 +20,34 @@ BEGIN
 	GROUP BY  Anio_Semana
 	ORDER BY Anio_Semana;
 
-
 END;
 $$
 LANGUAGE 'plpgsql';
 ----------------------------------------------------
 
 ----------------------------------------------------
---FUNCION 2 PARA LOS N ARTISTAS CON MAS VENTAS EN UN RANGO DE FECHAS
+-- FUNCION 2 PARA LOS N ARTISTAS CON MAS VENTAS EN UN RANGO DE FECHAS
+-- Recibe dos fechas y un int
 ----------------------------------------------------
 DROP FUNCTION IF EXISTS ARTISTRANGE;
+
+CREATE OR REPLACE FUNCTION ARTISTRANGE(inicial DATE, finale DATE, num int)
+RETURNS TABLE (artistid int, nombre VARCHAR(120), ventas bigint)
+AS $$
+BEGIN
+	
+	RETURN QUERY
+	SELECT artist.artistid, artist.name, COUNT(*) AS VENTAS
+	FROM invoiceline
+	JOIN invoice on invoiceline.invoiceid = invoice.invoiceid
+	JOIN track on invoiceline.trackid = track.trackid
+	JOIN album on track.albumid = album.albumid
+	JOIN artist on album.artistid = artist.artistid
+	WHERE invoicedate > $1 AND invoicedate < $2
+	GROUP BY artist.artistid
+	ORDER BY COUNT(*) DESC
+	LIMIT $3;
+
+END;
+$$
+LANGUAGE 'plpgsql';

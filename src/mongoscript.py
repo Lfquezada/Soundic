76b86d5d6316fef12503f62e3da29a6f3cac7ccc
JennsiS <42972809+JennsiS@users.noreply.github.com>
2020-05-27 13:59:23 -0600

    Update mongoscript.py

--- src/mongoscript.py
+++ src/mongoscript.py
@@ -16,7 +16,7 @@ cursor = connection.cursor()
 def purchases (date):
 	query = """
 		WITH purchases (date, genre, firstname, lastname, customerid) 
-		AS ( SELECT i.InvoiceDate AS date, g.name AS genre, c.FirstName, c.LastName, c.CustomerId
+		AS ( SELECT i.InvoiceDate AS date, g.name AS genre, c.FirstName, c.LastName, c.CustomerId, t.Name AS track
 			FROM InvoiceLine il
 			JOIN Invoice i ON il.InvoiceId= i.InvoiceId
 			JOIN Customer c ON i.CustomerId=c.CustomerId
@@ -32,6 +32,8 @@ def purchases (date):
 	for row in rows:
 		for key in cursor.description:
 			purchases_collection.insert_one({key[0]: value for value in row})
+	print('The purchases made in '+date+' have been added to purchases collection')
+
 
 
 #Writing json file with recent tracks 
@@ -49,19 +51,22 @@ def RecentTracks():
 	"""
 	cursor.execute(query)
 	rows = cursor.fetchall()
+	tracks_collection.delete_many( { } )
 	for row in rows:
 		for key in cursor.description:
 			tracks_collection.insert_one({key[0]: value for value in row})
+	print('New tracks have been added to Tracks collection')
 
 
-def recommendation(date):
+def recommendation():
 	genres=[]
 	clients=[]
+	tracks=[]
 	result=''
 	recommendation=[]
 
 	#Looking for clients in Mongo DB
-	query = purchases_collection.find({"row_to_json.date" : date }, {'row_to_json.genre':1, '_id':0}).limit(10)
+	query = purchases_collection.find({"row_to_json.date" : {'$gt': '2008-12-31TT00:00:00'} }, {'row_to_json.genre':1, '_id':0}).limit(10)
 	for i in query:
 		result=str(i)
 		f=result.split(':')
@@ -72,7 +77,7 @@ def recommendation(date):
 
 
 	#Looking for the purchased tracks genres by the clients in Mongo DB
-	query1 = purchases_collection.find({"row_to_json.date" : date}, {'row_to_json.firstname':1, '_id':0}).limit(10)
+	query1 = purchases_collection.find({"row_to_json.date" : {'$gt': '2008-12-31TT00:00:00'}}, {'row_to_json.firstname':1, '_id':0}).limit(10)
 	for i in query1:
 		result=str(i)
 		f=result.split(':')
@@ -81,19 +86,28 @@ def recommendation(date):
 		clients.append(h)
 	#print (clients)
 
+	query3 = purchases_collection.find({"row_to_json.date" : {'$gt': '2008-12-31TT00:00:00'}}, {'row_to_json.track':1, '_id':0}).limit(10)
+	for i in query3:
+		result=str(i)
+		f=result.split(':')
+		k=f[2]
+		h=((k.replace(" '","")).replace("}","")).replace("'","")
+		tracks.append(h)
+
 	#Recommendations for the users found in Mongo DB based on the tracks genres
 	contador=0
 	for i in genres:
 		query2=tracks_collection.find({"row_to_json.genre": i}, {'_id':0})
-		print ('\n Our recommendation for: '+ str (clients[contador])+ '\n')
-		print ('\n')
+		print ('\n Because you bought '+ str (tracks[contador])+ ' our recommendation for '+ str (clients[contador])+ '\n')
 		for j in query2:
 			print (j)
 		contador=contador+1
 		
 			
-#purchases('2010/2/8')
-#RecentTracks()
-recommendation('2010-02-08T00:00:00')
+#purchases('2013/9/2')
+#purchases('2013/12/4')
+#purchases('2010/02/08')
+RecentTracks()
+#recommendation()
 
 
